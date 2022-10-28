59998fb4dfc35b36304c0391929fdca58e2842a6
faustaslit <faustas.litvinaitis@student.nhlstenden.com>
2022-10-28 15:57:14 +0200

    Everything is merged. Still need to do some minor changes.

---
+++ php/display.php
@@ -0,0 +1,7 @@
+<?php
+function_alert("Thank you for writing to us, we will deliver your message");
+
+function function_alert($msg) {
+    echo "<script type='text/javascript'>alert('$msg');</script>";
+}
+?>
